610af0eb170d10478abec07fedc953b788064050
Gyeongho Yang <chchch1213@naver.com>
2024-03-29 17:14:39 +0900

    feat: 이전 움직임을 초기 보드에서 실행하는 로직 구현

--- src/main/java/chess/controller/ChessGame.java
+++ src/main/java/chess/controller/ChessGame.java
@@ -4,8 +4,10 @@ import chess.domain.board.Board;
 import chess.domain.board.BoardFactory;
 import chess.domain.game.command.Command;
 import chess.domain.game.command.CommandType;
+import chess.domain.game.state.End;
 import chess.domain.game.state.GameState;
 import chess.domain.game.state.Ready;
+import chess.repository.MoveDao;
 import chess.view.InputView;
 import chess.view.OutputView;
 
@@ -14,19 +16,40 @@ import java.util.function.Supplier;
 
 public class ChessGame {
 
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final MoveDao moveDao;
+
+    public ChessGame() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.moveDao = new MoveDao();
+    }
 
     public void run() {
         outputView.printStartMessage();
-        Board board = BoardFactory.createBoard();
+        Board board = prepareBoard();
+        GameState state = measureStateFromBoard(board);
 
-        GameState state = new Ready(board);
         while (!state.isEnd()) {
             GameState currentState = state;
             Command command = requestUntilValid(() -> Command.from(inputView.readCommand()));
             state = tryGet(() -> playAndPrint(currentState, command, board)).orElse(state);
         }
+    }
+
+    private Board prepareBoard() {
+        Board board = BoardFactory.createBoard();
+        moveDao.findAll()
+                .forEach(move -> board.move(move.source(), move.target()));
+        return board;
+    }
+
+    private GameState measureStateFromBoard(Board board) {
+        if (board.isKingCaptured()) {
+            return new End();
+        }
+        return new Ready(board);
     }
 
     private GameState playAndPrint(GameState state, Command command, Board board) {
