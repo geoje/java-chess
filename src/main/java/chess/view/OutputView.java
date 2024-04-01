5079d4ee0b12c02b601b95b3c965d7db30527dec
Gyeongho Yang <chchch1213@naver.com>
2024-04-01 16:49:23 +0900

    refactor: 운영체제간 출력 부분 고려 소개 문구 변경

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -11,17 +11,17 @@ public class OutputView {
     private static final int BOARD_SIZE = 8;
     private static final char EMPTY_PIECE = '.';
     private static final String TITLE_START = """
-            +-------------------------------------------------------+
-            | 우아한 체스 게임\t\t\t\t\t\t\t\t\t\t|
-            +-------------------------------------------------------+
-            | 게임 시작\t\t| start [white유저이름] [black유저이름]\t|
-            | 게임 재개\t\t| resume [방번호]\t\t\t\t\t\t|
-            | 방 리스트\t\t| room\t\t\t\t\t\t\t\t\t|
-            | 유저 정보\t\t| user [유저이름]\t\t\t\t\t\t|
-            | 이동\t\t\t| move [source위치] [target위치]\t\t\t|
-            | 승패 및 점수\t| status\t\t\t\t\t\t\t\t|
-            | 게임 종료\t\t| end\t\t\t\t\t\t\t\t\t|
-            +-------------------------------------------------------+
+            +-------------------------------------------------------
+            | 우아한 체스 게임
+            +-------------------------------------------------------
+            | 게임 시작: start [white유저이름] [black유저이름]
+            | 게임 재개: resume [방번호]
+            | 방 리스트: room
+            | 유저 정보: user [유저이름]
+            | 이동: move [source위치] [target위치]
+            | 승패 및 점수: status
+            | 게임 종료: end
+            +-------------------------------------------------------
                         
             """;
     private static final String ROOM_STATUS_FORMAT = "방 번호: %d / 흰색: %s / 검은색: %s%n";
